6a144f271ed48c2b34ef05361301d78fd14b935a
Ines Coelho <ines.opcoelho@gmail.com>
2019-07-23 08:16:55 +0100

    Create Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 
+import { Menu } from 'components/Menu';
+
 import './App.less';
 
+export const menuOptions = {
+  home: 'home',
+  about: 'about',
+  portfolio: 'portfolio',
+  talks: 'talks',
+  publications: 'publications'
+}
+
 export default class App extends React.Component{
+  state = { activeItem: menuOptions.home }
+
+  handleItemClick = (e: any, { name }: any) => this.setState({ activeItem: name })
+
   render() {
+    const { activeItem } = this.state;
 
     return (
       <div className="app">
+        <Menu
+          activeItem={activeItem}
+          handleItemClick={this.handleItemClick} />
       </div>
     );
   }
