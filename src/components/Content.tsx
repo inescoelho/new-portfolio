d936b9e8253e89fd718c5f00a7e44d2f00a05072
Ines Coelho <ines.opcoelho@gmail.com>
2019-07-26 09:06:00 +0100

    Add under construction information

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { menuOptions } from "App";
 import { Home } from 'components/Home';
 import { About } from 'components/About';
+import { Portfolio } from 'components/Portfolio';
 
 import 'components/Content.less';
 
@@ -14,5 +15,7 @@ export const Content: React.FC<Props> = ({ activeItem }) =>
   <>
     {activeItem === menuOptions.home && <div className="cont"><Home /></div>}
     {activeItem === menuOptions.about && <div className="cont"><About /></div>}
-    {activeItem === menuOptions.portfolio && <div className="cont">HELLO</div>}
+    {activeItem === menuOptions.portfolio && <div className="cont"><Portfolio /></div>}
+    {activeItem === menuOptions.publications && <div className="cont"><Portfolio /></div>}
+    {activeItem === menuOptions.talks && <div className="cont"><Portfolio /></div>}
   </>;
---
+++ src/components/Portfolio.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Segment } from 'semantic-ui-react';
+
+export const Portfolio: React.FC = () => {
+  return (
+    <Segment>
+      <div className={'textContainer'}>Under Construction</div>
+    </Segment>);
+}
